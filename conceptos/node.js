4c6d2980c3e93a9490cc382aec6dde49c8c64148
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2022-10-21 11:52:06 -0300

    Create node.js

---
+++ conceptos/node.js
@@ -0,0 +1,10 @@
+is a open source
+crossplatform
+javascript runtime environment
+entorno en tiempo de ejecucion
+multiplataforma
+para la capa del servidor
+basado en el lenguaje de programacion javascript
+asincrono
+con e/s de datos
+arquitectura orientada a objetos
